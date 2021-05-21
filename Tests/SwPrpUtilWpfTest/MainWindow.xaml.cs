49b8d2fe4f217f003a5abdea7301a716b9b09e77
zizu meridian <zizu.meridian@gmail.com>
2021-05-21 23:23:55 +0300

    Create progect structure

--- Tests/SwPrpUtilWpfTest/MainWindow.xaml.cs
+++ Tests/SwPrpUtilWpfTest/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SwPrpUtil
+namespace SwPrpUtilWpfTest
 {
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
