c65db097564790f1f798d31e9f257358ad35bed6
zizu meridian <zizu.meridian@gmail.com>
2021-06-01 11:26:47 +0300

    Create maket process SwPrpEditor 1

--- Model/SwPrpEditor.cs
+++ Model/SwPrpEditor.cs
@@ -8,6 +8,35 @@ namespace SwPrpUtil.Model
 {
 	class SwPrpEditor
 	{
-	
+		private List<SwFileItem> _swFileItems;
+		private List<SwProperty> _swSourceProperties;
+
+
+		public bool AddFolder(string pathToFolder) 
+		{
+			//Open folder
+			//Read files from folder
+			//Invoke AddFile for each files
+
+
+
+			throw new NotImplementedException();
+		}
+
+		public bool AddFile(string pathToFile)
+		{
+			//Create SwFileItem and add to _swFileItems/
+
+			throw new NotImplementedException();
+		}
+
+		public bool ImportPropertiesFromFile(string pathToFile, string configName = null)
+		{
+			throw new NotImplementedException();
+		}
+
+
+
+
 	}
 }
