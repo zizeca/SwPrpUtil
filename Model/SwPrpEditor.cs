43fad2f28a220c50d844e34b22d6813be661584f
zizu meridian <zizu.meridian@gmail.com>
2021-06-02 16:06:40 +0300

    implementation add methods

--- Model/SwPrpEditor.cs
+++ Model/SwPrpEditor.cs
@@ -1,38 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
+using System.ServiceModel.Configuration;
 
 namespace SwPrpUtil.Model
 {
-	class SwPrpEditor
+	internal class SwPrpEditor
 	{
 		private List<SwFileItem> _swFileItems;
 		private List<SwProperty> _swSourceProperties;
 
-
-		public bool AddFolder(string pathToFolder) 
+		public bool AddFolder(string pathToFolder)
 		{
-			//Open folder
-			//Read files from folder
-			//Invoke AddFile for each files
+			if (!Directory.Exists(pathToFolder))
+				throw new DirectoryNotFoundException(pathToFolder);
 
-			throw new NotImplementedException();
+			string[] files = Directory.GetFiles(pathToFolder);
+			if (files.Length == 0)
+			{
+				Debug.WriteLine(string.Format("Directory {0} has not files", pathToFolder));
+				return false;
+			}
+
+			foreach (string fileName in files)
+			{
+				if (!AddFile(fileName))
+					Debug.WriteLine(string.Format("Ignoring file {0}", fileName));
+			}
+
+			return true;
 		}
 
 		public bool AddFile(string pathToFile)
 		{
 			//Create SwFileItem and add to _swFileItems/
+			// get the file attributes for file or directory
+			if (!File.Exists(pathToFile))
+				throw new FileNotFoundException(pathToFile);
 
-			throw new NotImplementedException();
+			string extension = Path.GetExtension(pathToFile)?.ToLower();
+			if (extension == ".sldprt" || extension == ".sldasm" || extension == ".slddrw")
+			{
+				SwFileItem fileItem = new SwFileItem();
+				fileItem.FilePath = pathToFile;
+				_swFileItems.Add(fileItem);
+				return true;
+			}
+			else
+			{
+				return false;
+			}
 		}
 
 		public bool ImportPropertiesFromFile(string pathToFile, string configName = null)
 		{
+			//Run solidworks ( or get exist solidworks object)
 			//Open file in solidworks
-			//Read properties from main properti or from configName property
-			//create _swSourceProperties
+			//open a new window with the dialog - from where to import properties (main properties or configuration properties)
+			//read selected properties
+			//add properties to _swSourceProperties
 
 			throw new NotImplementedException();
 		}
@@ -44,8 +71,14 @@ namespace SwPrpUtil.Model
 			throw new NotImplementedException();
 		}
 
+		public void ClearFileList()
+		{
+			_swFileItems.Clear();
+		}
 
-
-
+		public void ClearPropertyList()
+		{
+			_swSourceProperties.Clear();
+		}
 	}
 }
