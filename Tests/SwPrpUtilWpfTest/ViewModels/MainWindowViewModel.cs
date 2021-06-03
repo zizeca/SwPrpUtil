afdac0193552578926b160f844edc61c990c1dbd
zizu meridian <zizu.meridian@gmail.com>
2021-06-03 10:58:27 +0300

    Rename mode to models 2

---
+++ Tests/SwPrpUtilWpfTest/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using SwPrpUtil.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwPrpUtil.ViewModels
+{
+	internal class MainWindowViewModel : ViewModelBase
+	{
+		#region Title
+
+		private string _Title = "Property Utils";
+
+		public string  Title
+		{
+			get => _Title;
+			set => Set(ref _Title, value);
+		}
+
+		#endregion
+
+	}
+}
