6c2082e24a9c1c31740908a2a6742b8c90457797
zizu meridian <zizu.meridian@gmail.com>
2021-06-09 00:25:20 +0300

    test num parser

--- Tests/SwPrpUtilConsole/Program.cs
+++ Tests/SwPrpUtilConsole/Program.cs
@@ -6,10 +6,45 @@ using System.Threading.Tasks;
 
 namespace SwPrpUtilConsole
 {
-	class Program
+	internal class Program
 	{
-		static void Main(string[] args)
+		public enum Testflag
 		{
+			none = 0,
+			one = 1,
+			two = 2,
+			four = 4,
+			eight = 8
+		}
+
+		private static Testflag f = Testflag.four | Testflag.one;
+		private static Testflag z = Testflag.four | Testflag.none;
+
+		private static void Main(string[] args)
+		{
+			IEnumerable<Enum> e1 = GetFlags(f);
+			IEnumerable<Enum> e2 = GetFlags(z);
+
+			Console.WriteLine("test flags f");
+			foreach (var item in e1)
+			{
+				Console.Write("{0} ,", item.ToString());
+			}
+
+			Console.WriteLine("test flags z");
+			foreach (var item in e2)
+			{
+				Console.Write("{0} ,", item.ToString());
+			}
+
+			Console.Read();
+		}
+
+		private static IEnumerable<Enum> GetFlags(Enum input)
+		{
+			foreach (Enum value in Enum.GetValues(input.GetType()))
+				if (input.HasFlag(value))
+					yield return value;
 		}
 	}
 }
